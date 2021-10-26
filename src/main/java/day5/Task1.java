1199853abfe620a1c747eb29c7c066965413cbd8
Fenix912 <maggie991@bk.ru>
2021-10-27 00:45:14 +0300

    add days 5

--- src/main/java/day5/Task1.java
+++ src/main/java/day5/Task1.java
@@ -2,6 +2,52 @@ package day5;
 
 public class Task1 {
     public static void main(String[] args) {
+        Car car1 = new Car();
+        car1.setModel("Porshe");
+        car1.setColor("Red");
+        car1.setGod(2020);
 
+        System.out.println("Модель машины: " + car1.getModel());
+        System.out.println("Цвет машины: " + car1.getColor());
+        System.out.println("Год машины: " + car1.getGod());
     }
 }
+    class Car {
+        private String model;
+        private String color;
+        private int god;
+
+        public void setModel(String userModel) {
+            if (userModel.isEmpty()){
+                System.out.println("Вы ввели пустую строку");
+            } else {
+            model = userModel;
+            }
+        }
+        public String getModel() {
+            return model;
+        }
+
+        public void setColor(String userColor) {
+            if (userColor.isEmpty()){
+                System.out.println("Вы ввели пустую строку");
+            } else {
+                color = userColor;
+            }
+        }
+        public String getColor() {
+            return color;
+        }
+
+        public void setGod(int userGod) {
+            if (userGod < 0) {
+                System.out.println("Год машины должен быть положительным числом");
+            } else {
+                god = userGod;
+            }
+        }
+        public int getGod() {
+            return god;
+        }
+    }
+
--- src/main/java/day5/Task2.java
+++ src/main/java/day5/Task2.java
@@ -3,6 +3,30 @@ package day5;
 
 public class Task2 {
     public static void main(String[] args) {
+        Motorbike motorbike1 = new Motorbike("BMV", "Blue", 2020);
+        System.out.println("Модель машины: " + motorbike1.getModel());
+        System.out.println("Цвет машины: " + motorbike1.getColor());
+        System.out.println("Год машины: " + motorbike1.getGod());
+    }
+}
+class Motorbike {
+    private String Model;
+    private String Color;
+    private int God;
 
+    public Motorbike(String Model, String Color, int God) {
+        this.Model = Model;
+        this.Color = Color;
+        this.God = God;
+    }
+
+    public String getModel() {
+        return Model;
+    }
+    public String getColor() {
+        return Color;
+    }
+    public int getGod() {
+        return God;
     }
 }
